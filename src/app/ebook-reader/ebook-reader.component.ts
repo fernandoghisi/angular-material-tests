0853a21930fbc228fc75b4dcf2ebfb89a8da8af3
Fernando Ghisi <fghisi@casan.com.br>
2017-03-06 11:03:05 -0300

    Inlcuding a "md-list" inside the sidenav

--- src/app/ebook-reader/ebook-reader.component.ts
+++ src/app/ebook-reader/ebook-reader.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EbookReaderComponent implements OnInit {
 
+  chapters = [
+    {number: 1, name: 'Introduction'},
+    {number: 2, name: 'First part'},
+    {number: 3, name: 'Second part'},
+    {number: 4, name: 'Conclusion'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
